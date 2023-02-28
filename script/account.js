9ebe4484b5f03a55a4e2575a516cb71f8ccd5613
KJ Jackson <kjjackson619@yahoo.com>
2023-02-27 20:26:09 -0500

    account.js

--- script/account.js
+++ script/account.js
@@ -3,5 +3,26 @@ const accountBtn = document.getElementById("accountBtn");
 accountBtn.addEventListener("click", function (e) {
     e.preventDefault();
 
+    var accountModal = document.getElementById("accountModal");
+
+    var login = document.getElementById("login");
+    var check = document.getElementById("check");
+    var signup = document.getElementById("signup");
+
+    login.addEventListener("click", function (e) {
+        e.preventDefault();
+
+    });
+
+    check.addEventListener("change", function (e) {
+        e.preventDefault();
+
+    });
+
+
+    signup.addEventListener("click", function (e) {
+        e.preventDefault();
+
+    });
 
 });
