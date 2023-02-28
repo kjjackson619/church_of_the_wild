aa7c8150b35135f8dc0b7161aefa5a2fd4a565cd
KJ Jackson <kjjackson619@yahoo.com>
2023-02-27 21:02:58 -0500

    account.js

--- script/account.js
+++ script/account.js
@@ -6,22 +6,33 @@ accountBtn.addEventListener("click", function (e) {
     var accountModal = document.getElementById("accountModal");
     accountModal.style.visibility = "visible";
     var login = document.getElementById("login");
-    var check = document.getElementById("check");
     var signup = document.getElementById("signup");
 
     login.addEventListener("click", function (e) {
         e.preventDefault();
+        var email = document.getElementById("email");
+        var password = document.getElementById("password");
 
+        console.log(email.value + password.value);
     });
 
     check.addEventListener("change", function (e) {
         e.preventDefault();
-
+        var check = document.getElementById("check");
+        if (check.checked === true) {
+            console.log(check.checked);
+            return;
+        }
+        else {
+            console.log(check.checked);
+            return false;
+        }
     });
 
 
     signup.addEventListener("click", function (e) {
         e.preventDefault();
+        var signup = document.getElementById("signup");
 
     });
 
