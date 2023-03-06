19e05b6076600c5a2b73b7f52959aa6c38fd2dea
KJ Jackson <kjjackson619@yahoo.com>
2023-03-05 20:30:41 -0500

    forgot

--- script/account.js
+++ script/account.js
@@ -1,54 +1,61 @@
 const accountBtn = document.getElementById("accountBtn");
-const signupSubmit = document.getElementById("create");
-const cancel = document.getElementById("cancel");
+const create = document.getElementById("create");
+const sendEmail = document.getElementById("send-email");
 
-accountBtn.addEventListener("click", function (e) {
-    e.preventDefault();
-
-    var accountModal = document.getElementById("accountModal");
-    accountModal.style.visibility = "visible";
-    var login = document.getElementById("login");
-
-
-    login.addEventListener("click", function (e) {
+var account = function () {
+    accountBtn.addEventListener("click", function (e) {
         e.preventDefault();
-        var email = document.getElementById("email");
-        var password = document.getElementById("password");
 
-        console.log(email.value + password.value);
+        var accountModal = document.getElementById("accountModal");
+        accountModal.style.visibility = "visible";
+        var login = document.getElementById("login");
+
+
+        login.addEventListener("click", function (e) {
+            e.preventDefault();
+            var email = document.getElementById("email");
+            var password = document.getElementById("password");
+
+            console.log(email.value + password.value);
+        });
+
+        check.addEventListener("change", function (e) {
+            e.preventDefault();
+            var check = document.getElementById("check");
+            if (check.checked === true) {
+                console.log(check.checked);
+                return;
+            }
+            else {
+                console.log(check.checked);
+                return false;
+            }
+        });
+
+
     });
 
-    check.addEventListener("change", function (e) {
+    create.addEventListener("click", function (e) {
         e.preventDefault();
-        var check = document.getElementById("check");
-        if (check.checked === true) {
-            console.log(check.checked);
+        var email = document.getElementById("new-email");
+        var password = document.getElementById("new-password");
+        var verify = document.getElementById("verify");
+
+        console.log(email.value + password.value + verify.value);
+
+        if (verify.value === password.value) {
+            console.log(true);
             return;
-        }
-        else {
-            console.log(check.checked);
-            return false;
+        } else {
+            console.log(false)
+            alert("Password's do not match!");
         }
     });
 
+    sendEmail.addEventListener("click", function (e) {
+        e.preventDefault();
+        var email = document.getElementById("forgot-email");
+        console.log(email.value);
+    })
 
-});
-
-create.addEventListener("click", function (e) {
-    e.preventDefault();
-    var email = document.getElementById("new-email");
-    var password = document.getElementById("new-password");
-    var verify = document.getElementById("verify");
-
-    console.log(email.value + password.value + verify.value);
-
-    if (verify.value === password.value) {
-        console.log(true);
-        return;
-    } else {
-        console.log(false)
-        alert("Password's do not match!");
-    }
-});
-
-
+};
